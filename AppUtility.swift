61488ce6dd1511347058e5636c493b324c5b62a0
Sean McCarthy <smccarthy@csumb.edu>
2017-09-17 12:02:00 -0700

    init commit

---
+++ AppUtility.swift
@@ -0,0 +1,30 @@
+//
+//  AppUtility.swift
+//  Roommate-Login
+//
+//  Created by Sean McCarthy on 9/17/17.
+//  Copyright © 2017 Sean McCarthy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct AppUtility {
+    
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+        
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            delegate.orientationLock = orientation
+        }
+    }
+    
+    /// OPTIONAL Added method to adjust lock and rotate to the desired orientation
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
+        
+        self.lockOrientation(orientation)
+        
+        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
+    }
+    
+}
